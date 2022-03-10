ccf1ee22a863c5b6c22fd9c787548d1b5f5cc79e
jeba <92013304+JebasinghYozy@users.noreply.github.com>
2022-03-10 09:31:46 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,12 +2,7 @@ public class Sample{
 
  public static void main(String[] args) {
   // TODO Auto-generated method stub
-<<<<<<< Updated upstream
     System.out.println("Welcome to Gitub");
-=======
-    System.out.println("Welcome to Gitub test");
-    System.out.println("Welcome to Gitub test");
->>>>>>> Stashed changes
 
  }
 
